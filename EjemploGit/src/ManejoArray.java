2fe22f7e52488a2b2717d7f75b99c33d868adad7
jjmtzg <jjmartinezg.04@gmail.com>
2017-10-16 18:54:16 -0500

    Haciendo meses estaticos

--- EjemploGit/src/ManejoArray.java
+++ EjemploGit/src/ManejoArray.java
@@ -1,13 +1,13 @@
-
-
+import static fechas.MesesYDias.MESES_ABREVIADOS;
+import static fechas.MesesYDias.SEM_ABREVIADOS;
 /*
  * Mostrar en enero julio noviembre diciembre y lunes viernes martes
  */
 public class ManejoArray {
 
 	public ManejoArray() {
-		String[] meses={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
-		String[] sem={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+		String[] meses=MESES_ABREVIADOS;
+		String[] sem=SEM_ABREVIADOS;
 		System.out.println(meses[0]+" "+meses[6]+" "+meses[10]+" "+meses[11]+" "+meses[2]+" "
 		+sem[0]+" "+sem[4]+" "+sem[1]);
 			//fallo provocado
---
+++ EjemploGit/src/fechas/MesesYDias.java
@@ -0,0 +1,9 @@
+package fechas;
+
+public class MesesYDias {
+	public static final String[] MESES_ABREVIADOS={"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+	public static final String[] SEM_ABREVIADOS={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
+	public static final String[] MESES={"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+	public static final String[] SEM={"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
+	
+}
