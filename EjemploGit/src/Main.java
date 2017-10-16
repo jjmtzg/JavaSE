41d92c0b1fbc41166b92b4c8c6cf1d6ce4f2712e
jjmtzg <jjmartinezg.04@gmail.com>
2017-10-16 15:54:53 -0500

    Agregando manejo de Strings

--- EjemploGit/src/Main.java
+++ EjemploGit/src/Main.java
@@ -1,17 +1,17 @@
 
 public class Main 
 {
-
 	public static void main(String[] args)
 	{
 		/*String cad1="",cad2="s";
 		int i=0,j=1,r;
 		float d=3.141592678f,c=10.3443434343f,rf;
 		System.out.println("Suma: "+(r=i+j)+"\n"+(j+d));*/
-		int a=8,b=81,c=2;
+		/*int a=8,b=81,c=2;
 		double x1,x2;
 		System.out.println((x1=((-b)+Math.sqrt((b^2)-(4*a*c)))/(2*a))+"\n"+
-				(x2=((-b)-Math.sqrt((b^2)-(4*a*c)))/(2*a)));
+				(x2=((-b)-Math.sqrt((b^2)-(4*a*c)))/(2*a)));*/
+		ManejoString s=new ManejoString();
 	}
 
 }
---
+++ EjemploGit/src/ManejoString.java
@@ -0,0 +1,20 @@
+
+public class ManejoString {
+
+	ManejoString()
+	{
+		String cadena="Parangaricutirimicuaro";
+		System.out.println(cadena);
+			//Criptografía Cesar
+		String cadenaCesar="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+		int espacio=3;//Delimitamos a partir de donde cambia
+		for(int i=0;i<cadena.length();i++)
+		{
+			System.out.println(cadena.charAt(i));//Observamos cambios
+			cadena=(cadena.replace(cadena.charAt(i), cadenaCesar.charAt(i+espacio)));//Guardamos en la cadena cada cambio
+		}
+		System.out.println(cadena);
+		System.out.println(cadena.substring(0,2)+cadena.substring(2, 6).toLowerCase()+cadena.substring(6));
+		System.out.println(cadena.toLowerCase());
+	}
+}
