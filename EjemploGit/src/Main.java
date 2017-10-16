a17ae6882ddeb4f0fce70da0d7bf2ecd48c5a22f
jjmtzg <jjmartinezg.04@gmail.com>
2017-10-16 15:27:23 -0500

    Chicharrones al 3x2

--- EjemploGit/src/Main.java
+++ EjemploGit/src/Main.java
@@ -1,10 +1,17 @@
 
-public class Main {
+public class Main 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola Git");
-		
+	public static void main(String[] args)
+	{
+		/*String cad1="",cad2="s";
+		int i=0,j=1,r;
+		float d=3.141592678f,c=10.3443434343f,rf;
+		System.out.println("Suma: "+(r=i+j)+"\n"+(j+d));*/
+		int a=8,b=81,c=2;
+		double x1,x2;
+		System.out.println((x1=((-b)+Math.sqrt((b^2)-(4*a*c)))/(2*a))+"\n"+
+				(x2=((-b)-Math.sqrt((b^2)-(4*a*c)))/(2*a)));
 	}
 
 }
